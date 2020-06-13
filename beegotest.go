926fbab218cc07052d273e93f3d86840e83008d1
james <james9203@163.com>
2020-06-13 12:57:45 +0800

    change

---
+++ beegotest.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/astaxie/beego"
+
+type HomeController struct {
+	beego.Controller
+}
+
+func (this *HomeController) Get()  {
+	this.Ctx.WriteString("Hello World!")
+}
+func main()  {
+	beego.Router("/",&HomeController{})
+	beego.Run()
+}
